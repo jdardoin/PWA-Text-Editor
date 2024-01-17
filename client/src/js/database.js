f5e018d94513f4887f10477172c2f50dc145d80f
jdardoin <jdardoin@icloud.com>
2024-01-16 22:03:47 -0600

    updated database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+const db = await openDB('jate', 1);
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
